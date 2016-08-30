45260ebe70ff9e3ff508c9820901ce1e7a072d01
Ben Murphy <ben@constellation.software>
2016-08-30 16:59:52 -0400

    Create a ResultsViewController
    Embed in a container view

--- Etta/SearchViewController.swift
+++ Etta/SearchViewController.swift
@@ -13,15 +13,24 @@ class SearchViewController: UIViewController, SearchControllerDelegate {
 
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var searchBox: UITextField!
-    @IBOutlet weak var resultTableView: UITableView!
+    @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var searchToResultsSpacingConstraint: NSLayoutConstraint!
     @IBOutlet weak var searchBoxHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var stackViewCenterVConstraint: NSLayoutConstraint!
     @IBOutlet weak var searchBoxTopConstraint: NSLayoutConstraint!
 
+    /// Add the resultsViewController from the storyboard as a child view controller
+    lazy var resultsViewController: ResultsViewController = {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        var viewController = storyboard.instantiateViewController(withIdentifier: "ResultsViewController") as! ResultsViewController
+        viewController.delegate = self
+        self.addViewControllerAsChildViewController(viewController)
+        return viewController
+    }()
+
     var entries: [HTMLDictionaryEntry] = [] {
         didSet {
-            resultTableView.reloadData()
+            resultsViewController.resultsTableView.reloadData()
         }
     }
 
@@ -32,9 +41,10 @@ class SearchViewController: UIViewController, SearchControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureTableView()
+
         observeKeyboard()
         positionSearchInCenter()
+        configureSearchBox()
         searchController.delegate = self
 
     }
@@ -44,11 +54,25 @@ class SearchViewController: UIViewController, SearchControllerDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    private func addViewControllerAsChildViewController(_ viewController: UIViewController) {
+        // Add Child View Controller
+        addChildViewController(viewController)
+
+        // Add Child View as Subview
+        containerView.addSubview(viewController.view)
+
+        // Configure Child View
+        viewController.view.frame = view.bounds
+        viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        // Notify Child View Controller
+        viewController.didMove(toParentViewController: self)
+    }
+
     // MARK: - Methods
-    func configureTableView() {
-        resultTableView.dataSource = self
-        resultTableView.estimatedRowHeight = 150
-        resultTableView.rowHeight = UITableViewAutomaticDimension
+
+    func configureSearchBox() {
+        searchBox.clearButtonMode = .always
     }
 
 
@@ -67,65 +91,11 @@ class SearchViewController: UIViewController, SearchControllerDelegate {
         searchController.lookUp(term)
     }
 
-    /// When the text view is tapped determine if the tap hit a link.
-    /// If so, perform a search using that term.
-    ///
-    /// - parameter sender: the textview tapped.
-    @IBAction func textTapped(_ sender: UITapGestureRecognizer) {
-        guard let textView = (sender.view as? UITextView) else {
-            return
-        }
-
-        let layoutManager = textView.layoutManager
-        var location: CGPoint = sender.location(in: textView)
-        location.x -= textView.textContainerInset.left
-        location.y -= textView.textContainerInset.top
-
-        let charIndex = layoutManager.characterIndex(for: location, in: textView.textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
-
-        guard charIndex < textView.textStorage.length else {
-            return
-        }
-
-        var range = NSRange(location: 0, length: 0)
-
-        guard (textView.attributedText?.attribute("SearchText", at: charIndex, effectiveRange: &range) as? NSString) != nil else {
-            return
-        }
-
-        let tappedTerm = (textView.attributedText.string as NSString).substring(with: range)
-
-        search(tappedTerm)
-    }
 
 
 }
 
 
-// MARK: - Table View Protocol Conformance
-extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
-
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return entries.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "EttaResultCell", for: indexPath) as! EttaResultTableViewCell
-        let entry = entries[indexPath.item]
-        cell.term.text = entry.termText()
-        cell.links = entry.linkedText()
-        cell.linksList.text = cell.links.joined(separator: ", ")
-        cell.entryDescription.attributedText = entry.descriptionWithLinks()
-
-        /// Add gesture recognizer
-        let tapEvent = UITapGestureRecognizer(target: self, action: #selector(SearchViewController.textTapped(_:)))
-        cell.entryDescription.addGestureRecognizer(tapEvent)
-        return cell
-    }
-}
 
 // MARK: - Animations
 extension SearchViewController {
---
+++ ResultsViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ResultsViewController.swift
+//  Etta
+//
+//  Created by Ben Murphy on 8/30/16.
+//  Copyright © 2016 Constellation Software. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var resultsTableView: UITableView!
+
+    var delegate: SearchControllerDelegate!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTableView()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func configureTableView() {
+        resultsTableView.dataSource = self
+        resultsTableView.estimatedRowHeight = 150
+        resultsTableView.rowHeight = UITableViewAutomaticDimension
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+// MARK: - Table View Protocol Conformance
+extension ResultsViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return delegate.entries.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EttaResultCell", for: indexPath) as! EttaResultTableViewCell
+        let entry = delegate.entries[indexPath.item]
+        cell.term.text = entry.termText()
+        cell.links = entry.linkedText()
+        cell.linksList.text = cell.links.joined(separator: ", ")
+        cell.entryDescription.attributedText = entry.descriptionWithLinks()
+
+        /// Add gesture recognizer
+        let tapEvent = UITapGestureRecognizer(target: self, action: #selector(ResultsViewController.textTapped(_:)))
+        cell.entryDescription.addGestureRecognizer(tapEvent)
+        return cell
+    }
+
+    /// When the text view is tapped determine if the tap hit a link.
+    /// If so, perform a search using that term.
+    ///
+    /// - parameter sender: the textview tapped.
+    @IBAction func textTapped(_ sender: UITapGestureRecognizer) {
+        guard let textView = (sender.view as? UITextView) else {
+            return
+        }
+
+        let layoutManager = textView.layoutManager
+        var location: CGPoint = sender.location(in: textView)
+        location.x -= textView.textContainerInset.left
+        location.y -= textView.textContainerInset.top
+
+        let charIndex = layoutManager.characterIndex(for: location, in: textView.textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+
+        guard charIndex < textView.textStorage.length else {
+            return
+        }
+
+        var range = NSRange(location: 0, length: 0)
+
+        guard (textView.attributedText?.attribute("SearchText", at: charIndex, effectiveRange: &range) as? NSString) != nil else {
+            return
+        }
+
+        let tappedTerm = (textView.attributedText.string as NSString).substring(with: range)
+        
+        (parent as? SearchViewController)?.search(tappedTerm)
+    }
+
+}
+
