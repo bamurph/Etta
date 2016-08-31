0d4c48cb1b2c9809e503ed32e7623abea1637f31
Ben Murphy <ben@constellation.software>
2016-08-31 17:04:48 -0400

    Basic history page working

--- HistoryViewController.swift
+++ HistoryViewController.swift
@@ -11,16 +11,35 @@ import UIKit
 class HistoryViewController: UIViewController {
 
     @IBOutlet weak var historyTableView: UITableView!
-    
+
     var delegate: SearchControllerDelegate!
     var log = [Record]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("~ LOADED HISTORY VC")
         log = delegate.history()
+        historyTableView.delegate = self
+        historyTableView.dataSource = self
+
     }
 
 
+}
 
+extension HistoryViewController: UITableViewDataSource, UITableViewDelegate {
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return log.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryCell", for: indexPath as IndexPath)
+        cell.textLabel?.text = log[indexPath.item].term
+        return cell
+    }
 }
